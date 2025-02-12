1d8ef272800a0f875fe29be37c0a9dbff51888b1
sun <sunyibing@outlook.com>
2025-02-12 17:20:26 +0800

    修正代码

--- 1.同步分析报告至当前目录.sh
+++ 1.同步分析报告至当前目录.sh
@@ -1,8 +1,25 @@
 #!/bin/bash
+set -u
+
+: ${DIALOG=dialog}
+: ${DIALOG_OK=0}
+: ${DIALOG_CANCEL=1}
+: ${DIALOG_HELP=2}
+: ${DIALOG_EXTRA=3}
+: ${DIALOG_ITEM_HELP=4}
+: ${DIALOG_ESC=255}
+
+: ${SIG_HUP=1}
+: ${SIG_INT=2}
+: ${SIG_QUIT=3}
+: ${SIG_KILL=9}
+: ${SIG_TERM=15}
+
 dir_src="../WH投资数据分析系统-Dev开发环境/Runtime/Analyse"
 dir_des="Analyse分析报告"
 if [[ ! -d $dir_src ]];then
-    echo -e "\033[31m 请检查 $dir_src 是否存在!\033[0m"
+    $DIALOG --title "WH投资数据分析系统" --clear \
+        --msgbox "请检查: $dir_src 是否存在!" 10 41
     exit 1
 fi
 if [[ ! -d $dir_des ]];then
